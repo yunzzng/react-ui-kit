61c3831a6c5107cd58714178c062bcb8f9835ae3
yunzzng <leejiyun0114@naver.com>
2025-04-17 20:43:32 +0900

    update carousel

--- src/components/Carousel/CarouselIndicator.tsx
+++ src/components/Carousel/CarouselIndicator.tsx
@@ -3,26 +3,19 @@ import useCarouselContext from "./hooks/useCarouselContext";
 
 interface CarouselIndicatorProps {
   className?: string;
-  activeColor?: string;
-  defaultColor?: string;
 }
 
-const CarouselIndicator: FC<CarouselIndicatorProps> = ({ className, activeColor, defaultColor }) => {
+const CarouselIndicator: FC<CarouselIndicatorProps> = ({ className }) => {
   const { carouselIndex, setCarouselIndex, itemLength } = useCarouselContext();
 
   const indexes = useMemo(() => Array.from({ length: itemLength }, (_, index) => index), [itemLength]);
 
-  const carouselIndicatorCls = useMemo(() => {
-    return className ? className : "";
-  }, [className]);
-
   return (
-    <div className={carouselIndicatorCls}>
+    <div className={className}>
       {indexes.map((index) => (
         <button
           key={index}
-          className={`${index === carouselIndex ? "active" : ""} ${className || ""}`.trim()}
-          style={{ backgroundColor: index === carouselIndex ? activeColor : defaultColor }}
+          className={index === carouselIndex ? "active" : ""}
           onClick={() => setCarouselIndex(index)}
         />
       ))}
